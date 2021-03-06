b190513c8a60f281e13e9f8e4e0c0c87b57101c1
rick <1450685+LinuxSuRen@users.noreply.github.com>
2021-03-06 22:44:30 +0800

    Add comments for export functions

--- pkg/retry_client.go
+++ pkg/retry_client.go
@@ -5,12 +5,14 @@ import (
 	"net/url"
 )
 
+// RetryClient is the wrap of http.Client
 type RetryClient struct {
 	http.Client
 	MaxAttempts     int
 	currentAttempts int
 }
 
+// Do is the wrap of http.Client.Do
 func (c *RetryClient) Do(req *http.Request) (rsp *http.Response, err error) {
 	rsp, err = c.Client.Do(req)
 	if _, ok := err.(*url.Error); ok {
