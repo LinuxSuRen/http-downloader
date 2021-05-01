c96ac5bcbd5db98c8f306fe87d80ee5be07b7878
Zhao Xiaojie <1450685+LinuxSuRen@users.noreply.github.com>
2021-05-01 18:58:54 +0800

    Fetch config git repo using go-git to replace git cli (#61)

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -5,6 +5,7 @@ import (
 	"path"
 	"strings"
 
+	"github.com/go-git/go-git/v5"
 	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 )
@@ -39,11 +40,25 @@ func fetchLatestRepo(repo string, branch string) (err error) {
 	}
 
 	if ok, _ := pathExists(configDir); ok {
-		err = execCommandInDir("git", configDir, "pull", "origin", branch)
-	} else {
-		if err = os.MkdirAll(configDir, 0644); err == nil {
-			err = execCommand("git", "clone", "--depth", "1", repo, configDir)
+		var repo *git.Repository
+		if repo, err = git.PlainOpen(""); err == nil {
+			var wd *git.Worktree
+			if wd, err = repo.Worktree(); err == nil {
+				if err = wd.Pull(&git.PullOptions{
+					RemoteName: "origin",
+					Progress:   os.Stdout,
+					Force:      true,
+				}); err != nil && err != git.NoErrAlreadyUpToDate {
+					return
+				}
+				err = nil
+			}
 		}
+	} else {
+		_, err = git.PlainClone(configDir, false, &git.CloneOptions{
+			URL:      repo,
+			Progress: os.Stdout,
+		})
 	}
 
 	if err != nil && strings.Contains(err.Error(), "exit status 128") {
