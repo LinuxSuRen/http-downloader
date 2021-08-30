a84c3612e93425fddc811ada694234ff8b7c3c8f
rick <1450685+LinuxSuRen@users.noreply.github.com>
2021-08-30 23:02:10 +0800

    Add support to install vim via brew

---
+++ pkg/os/brew/init.go
@@ -0,0 +1,8 @@
+package brew
+
+import "github.com/linuxsuren/http-downloader/pkg/os/core"
+
+// SetInstallerRegistry sets the installer of registry
+func SetInstallerRegistry(registry core.InstallerRegistry) {
+	registry.Registry("vim", &vimInstallerInMacOS{})
+}
