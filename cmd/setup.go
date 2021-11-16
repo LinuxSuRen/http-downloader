597e7b2d915b81b637e488073d15e41f6380c7e5
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2021-11-16 08:42:50 +0800

    Add setup command to init the config (#184)

---
+++ cmd/setup.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func newSetupCommand() (cmd *cobra.Command) {
+	opt := &setupOption{}
+	cmd = &cobra.Command{
+		Use:   "setup",
+		Short: "Init the configuration of hd",
+		RunE:  opt.runE,
+	}
+	return
+}
+
+type setupOption struct {
+}
+
+func (o *setupOption) runE(cmd *cobra.Command, args []string) (err error) {
+	selector := &survey.Select{
+		Message: "Select proxy-github",
+		Options: []string{"gh.api.99988866.xyz", "ghproxy.com", "mirror.ghproxy.com", ""},
+	}
+
+	var choose string
+	if err = survey.AskOne(selector, &choose); err == nil {
+		viper.Set("proxy-github", choose)
+	} else {
+		return
+	}
+
+	err = viper.WriteConfig()
+	return
+}
